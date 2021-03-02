1dce08015ec8f29740211315bed7d8e5274f2c48
viragjainedu <virag.j@somaiya.edu>
2021-03-02 12:40:55 +0530

    first commit

---
+++ 1.3.py
@@ -0,0 +1,17 @@
+# O(n) time .Use functionality i.e all values lie between 0 to n (abs(arr[i]))
+
+def printTwoElements( arr, size):
+	for i in range(size):
+		if arr[abs(arr[i])-1] > 0:
+			arr[abs(arr[i])-1] = -arr[abs(arr[i])-1]
+		else:
+			print("The repeating element is", abs(arr[i]))
+			
+	for i in range(size):
+		if arr[i]>0:
+			print("and the missing element is", i + 1)
+
+# Driver program to test above function */
+arr = [7, 3, 4, 5, 5, 6, 2]
+n = len(arr)
+printTwoElements(arr, n)
---
+++ 1.6.py
@@ -0,0 +1,36 @@
+# in O(n Log n) time and O(1) extra space 
+def mergeIntervals(arr): 
+		
+		# Sorting based on the increasing order 
+		# of the start intervals 
+		arr.sort(key = lambda x: x[0]) 
+		
+		# array to hold the merged intervals 
+		m = [] 
+		s = -10000
+		max = -100000
+		for i in range(len(arr)): 
+			a = arr[i] 
+			if a[0] > max: 
+				if i != 0: 
+					m.append([s,max]) 
+				max = a[1] 
+				s = a[0] 
+			else: 
+				if a[1] >= max: 
+					max = a[1] 
+		
+		#'max' value gives the last point of 
+		# that particular interval 
+		# 's' gives the starting point of that interval 
+		# 'm' array contains the list of all merged intervals 
+
+		if max != -100000 and [s, max] not in m: 
+			m.append([s, max]) 
+		print("The Merged Intervals are :", end = " ") 
+		for i in range(len(m)): 
+			print(m[i], end = " ") 
+
+# Driver code 
+arr = [[6, 8], [1, 9], [2, 4], [4, 7]] 
+mergeIntervals(arr) 
